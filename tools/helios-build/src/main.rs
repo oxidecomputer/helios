81ffeca9167176b90e9d645cafec73c4de8f1919
Joshua M. Clulow <jmc@oxide.computer>
2020-09-08 14:41:40 -0700

    part way to a sequenced build of all dependencies of a package

--- tools/helios-build/src/main.rs
+++ tools/helios-build/src/main.rs
@@ -4,11 +4,12 @@ use common::*;
 pub mod illumos;
 pub mod ensure;
 
-use anyhow::{Result, bail};
+use anyhow::{Result, bail, anyhow};
 use serde::{Serialize, Deserialize};
-use std::collections::{BTreeMap, HashMap};
+use std::collections::{BTreeMap, HashMap, VecDeque, BTreeSet};
 use std::process::Command;
 use std::io::{BufWriter, BufReader, Write};
+use std::fs::File;
 use slog::Logger;
 use illumos::ZonesExt;
 
@@ -161,6 +162,7 @@ fn cmd_build(log: &Logger, args: &[&str]) -> Result<()> {
             "mounted" => (true, false, true, true),
             "running" => (false, true, true, true),
             "installed" => (false, false, true, true),
+            "incomplete" => (false, false, true, true),
             "configured" => (false, false, false, true),
             n => bail!("unexpected zone state: {}", n),
         };
@@ -186,15 +188,7 @@ fn cmd_build(log: &Logger, args: &[&str]) -> Result<()> {
     /*
      * Make sure the metadata is up-to-date for this component.
      */
-    let targetdir = top_path(&["projects", "userland", "components",
-        &target])?;
-    ensure::run_utf8(log, &["/usr/bin/gmake", "-s",
-        "-C", targetdir.to_str().unwrap(),
-        "update-metadata"])?;
-    let mut mdf = targetdir.clone();
-    mdf.push("pkg5");
-    let f = std::fs::File::open(&mdf)?;
-    let umd: UserlandMetadata = serde_json::from_reader(&f)?;
+    let umd = read_metadata(log, &target)?;
 
     info!(log, "creating...");
     illumos::zone_create(bzn, format!("/zones/{}", bzn), "nlipkg")?;
@@ -211,7 +205,26 @@ fn cmd_build(log: &Logger, args: &[&str]) -> Result<()> {
     /*
      * Before booting the zone, we must make sure that we have installed all of
      * the required packages for this build.
+     *
+     * First, add our local build repository to the ephemeral zone so that we
+     * are preferring packages we may have rebuilt locally.
      */
+    let repodir = top_path(&["projects", "userland", "i386", "repo"])?;
+    ensure::run(log, &["pfexec", "/usr/bin/pkg", "-R", &bzr,
+        "set-publisher",
+        "-g", &format!("file://{}", repodir.to_str().unwrap()),
+        "--sticky",
+        "--search-first",
+        "userland"])?;
+    ensure::run(log, &["pfexec", "/usr/bin/pkg", "-R", &bzr,
+        "set-publisher",
+        "--non-sticky",
+        "openindiana.org"])?;
+    ensure::run(log, &["pfexec", "/usr/bin/pkg", "-R", &bzr,
+        "uninstall",
+        "userland-incorporation",
+        "entire"])?;
+
     let mut install = Vec::new();
     for dep in umd.dependencies.iter() {
         info!(log, "checking for {}...", dep);
@@ -274,13 +287,17 @@ fn cmd_build(log: &Logger, args: &[&str]) -> Result<()> {
         "svc:/milestone/multi-user-server:default")?;
 
     let archive = top_path(&["cache", "userland-archive"])?;
+    let targetdir = top_path(&["projects", "userland", "components",
+        &target])?;
     let buildscript = format!("#!/bin/bash\n\
         set -o errexit\n\
         set -o pipefail\n\
         set -o xtrace\n\
         export USERLAND_ARCHIVES='{}/'\n\
+        export COMPONENT_BUILD_ARGS='-j4'\n\
         cd '{}'\n\
-        /usr/bin/gmake publish\n",
+        /usr/bin/gmake publish\n
+        /usr/bin/gmake sample-manifest\n",
         archive.to_str().unwrap(),
         targetdir.to_str().unwrap());
     let spath = illumos::zone_deposit_script(bzn, &buildscript)?;
@@ -349,7 +366,7 @@ fn cmd_archive(log: &Logger, args: &[&str]) -> Result<()> {
 
     let datafile = top_path(&["cache", "assets.json"])?;
     let data: BTreeMap<String, Asset> =
-        serde_json::from_reader(std::fs::File::open(&datafile)?)?;
+        serde_json::from_reader(File::open(&datafile)?)?;
 
     let mut missing = Vec::new();
 
@@ -417,7 +434,7 @@ fn cmd_download_metadata(log: &Logger, args: &[&str]) -> Result<()> {
     let datafile = top_path(&["cache", "assets.json"])?;
 
     let mut data: BTreeMap<String, Asset> =
-        if let Ok(f) = std::fs::File::open(&datafile) {
+        if let Ok(f) = File::open(&datafile) {
             let r = BufReader::new(f);
             serde_json::from_reader(r)?
         } else {
@@ -440,6 +457,335 @@ fn cmd_download_metadata(log: &Logger, args: &[&str]) -> Result<()> {
     let mut w = BufWriter::new(f);
     serde_json::to_writer_pretty(&mut w, &data)?;
     w.flush()?;
+
+    Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+struct UserlandMapping {
+    fmri: String,
+    name: String,
+    path: String,
+}
+
+fn read_metadata(log: &Logger, target: &str) -> Result<UserlandMetadata> {
+    let targetdir = top_path(&["projects", "userland", "components",
+        &target])?;
+    ensure::run_utf8(log, &["/usr/bin/gmake", "-s",
+        "-C", targetdir.to_str().unwrap(),
+        "update-metadata"])?;
+    let mut mdf = targetdir.clone();
+    mdf.push("pkg5");
+    let f = File::open(&mdf)?;
+    Ok(serde_json::from_reader(&f)?)
+}
+
+#[derive(Debug, Deserialize)]
+struct PkgRepoList {
+    branch: String,
+    #[serde(rename = "build-release")]
+    build_release: String,
+    name: String,
+    publisher: String,
+    release: String,
+    timestamp: String,
+    version: String,
+    #[serde(rename = "pkg.fmri")]
+    fmri: String,
+}
+
+fn repo_contains(log: &Logger, fmri: &str) -> Result<bool> {
+    let repodir = top_path(&["projects", "userland", "i386", "repo"])?;
+
+    info!(log, "checking build for {}...", fmri);
+    let out = Command::new("/usr/bin/pkgrepo")
+        .env_clear()
+        .arg("list")
+        .arg("-F")
+        .arg("json-formatted")
+        .arg("-s")
+        .arg(&repodir.to_str().unwrap())
+        .arg(fmri)
+        .output()?;
+
+    if !out.status.success() {
+        let err = String::from_utf8_lossy(&out.stderr);
+        if !err.contains("did not match any packages") {
+            bail!("pkgrepo list failed: {}", out.info());
+        }
+    }
+
+    let pkgs: Vec<PkgRepoList> = serde_json::from_slice(&out.stdout)?;
+
+    if pkgs.is_empty() {
+        info!(log, "found no versions");
+        Ok(false)
+    } else {
+        for pkg in pkgs.iter() {
+            info!(log, "found version {}", pkg.version);
+        }
+        Ok(true)
+    }
+}
+
+#[derive(Debug)]
+struct ActionDepend {
+    fmri: String,
+    type_: String,
+}
+
+#[derive(Debug)]
+enum Action {
+    Depend(ActionDepend),
+    Unknown(String, Vec<String>, HashMap<String, String>),
+}
+
+#[derive(Debug)]
+enum ParseState {
+    Rest,
+    Type,
+    Key,
+    Value,
+    ValueQuoted,
+    ValueQuotedSpace,
+    ValueUnquoted,
+}
+
+fn parse_manifest(log: &Logger, input: &str) -> Result<Vec<Action>> {
+    let mut out = Vec::new();
+
+    for l in input.lines() {
+        let mut s = ParseState::Rest;
+        let mut a = String::new();
+        let mut k = String::new();
+        let mut v = String::new();
+        let mut vals: HashMap<String, String> = HashMap::new();
+        let mut free: Vec<String> = Vec::new();
+
+        for c in l.chars() {
+            match s {
+                ParseState::Rest => {
+                    if c.is_ascii_alphabetic() {
+                        a.clear();
+                        k.clear();
+                        v.clear();
+
+                        a.push(c);
+                        s = ParseState::Type;
+                    } else {
+                        bail!("invalid line ({:?}): {}", s, l);
+                    }
+                }
+                ParseState::Type => {
+                    if c.is_ascii_alphabetic() {
+                        a.push(c);
+                    } else if c == ' ' {
+                        s = ParseState::Key;
+                    } else {
+                        bail!("invalid line ({:?}): {}", s, l);
+                    }
+                }
+                ParseState::Key => {
+                    if c.is_ascii_alphanumeric()
+                        || c == '.' || c == '-' || c == '_'
+                    {
+                        k.push(c);
+                    } else if c == ' ' {
+                        free.push(k.clone());
+                        k.clear();
+                    } else if c == '=' {
+                        s = ParseState::Value;
+                    } else {
+                        bail!("invalid line ({:?}, {}): {}", s, k, l);
+                    }
+                }
+                ParseState::Value => {
+                    v.clear();
+                    if c == '"' {
+                        s = ParseState::ValueQuoted;
+                    } else {
+                        s = ParseState::ValueUnquoted;
+                        v.push(c);
+                    }
+                }
+                ParseState::ValueQuoted => {
+                    if c == '\\' {
+                        /*
+                         * XXX handle escaped quotes...
+                         */
+                        bail!("invalid line (backslash...): {}", l);
+                    } else if c == '"' {
+                        s = ParseState::ValueQuotedSpace;
+                    }
+                }
+                ParseState::ValueQuotedSpace => {
+                    if c == ' ' {
+                        vals.insert(k.to_string(), v.to_string());
+                        s = ParseState::Key;
+                        k.clear();
+                    } else {
+                        bail!("invalid line ({:?}): {}", s, l);
+                    }
+                }
+                ParseState::ValueUnquoted => {
+                    if c == '"' {
+                        bail!("invalid line (errant quote...): {}", l);
+                    } else if c == ' ' {
+                        vals.insert(k.to_string(), v.to_string());
+                        s = ParseState::Key;
+                        k.clear();
+                    } else {
+                        v.push(c);
+                    }
+                }
+            }
+        }
+
+        match s {
+            ParseState::ValueQuotedSpace | ParseState::ValueUnquoted => {
+                vals.insert(k.to_string(), v.to_string());
+            }
+            ParseState::Type => {},
+            _ => bail!("invalid line (terminal state {:?}: {}", s, l),
+        }
+
+        match a.as_str() {
+            "depend" => {
+                let fmri = vals.remove("fmri").ok_or(anyhow!("missing fmri"))?
+                    .clone();
+                let type_ = vals.remove("type").ok_or(anyhow!("missing type"))?
+                    .clone();
+
+                if !vals.is_empty() {
+                    bail!("{} line had extra properties: {:?}", a, vals);
+                }
+
+                out.push(Action::Depend(ActionDepend {
+                    fmri,
+                    type_,
+                }))
+            }
+            _ => out.push(Action::Unknown(a.to_string(), free, vals)),
+        }
+    }
+
+    Ok(out)
+}
+
+fn repo_contents(log: &Logger, fmri: &str) -> Result<Vec<Action>> {
+    let repodir = top_path(&["projects", "userland", "i386", "repo"])?;
+
+    info!(log, "checking contents for {}...", fmri);
+    let out = Command::new("/usr/bin/pkgrepo")
+        .env_clear()
+        .arg("contents")
+        .arg("-m")
+        .arg("-s")
+        .arg(&repodir.to_str().unwrap())
+        .arg(fmri)
+        .output()?;
+
+    if !out.status.success() {
+        bail!("pkgrepo contents failed: {}", out.info());
+    }
+
+    /*
+     * Parse the output manifest lines...
+     */
+    Ok(parse_manifest(log, &String::from_utf8(out.stdout)?)?)
+}
+
+fn cmd_userland_plan(log: &Logger, args: &[&str]) -> Result<()> {
+    let opts = baseopts();
+
+    let usage = || {
+        println!("{}", opts.usage("Usage: helios [OPTIONS] \
+            userland-plan [OPTIONS]"));
+    };
+
+    let res = opts.parse(args)?;
+
+    if res.opt_present("help") {
+        usage();
+        return Ok(());
+    }
+
+    if res.free.is_empty() {
+        bail!("provide package names");
+    }
+
+    let top = top()?;
+    println!("helios repository root is: {}", top.display());
+
+    /*
+     * Load the FMRI-to-path mapping file:
+     */
+    let mdf = top_path(&["projects", "userland", "components",
+        "mapping.json"])?;
+    let f = File::open(&mdf)?;
+    let um: Vec<UserlandMapping> = serde_json::from_reader(&f)?;
+
+    let mut q: VecDeque<String> = VecDeque::new();
+    let mut seen: BTreeSet<String> = BTreeSet::new();
+
+    for pkg in res.free.iter() {
+        q.push_back(pkg.clone());
+    }
+
+    while let Some(pkg) = q.pop_front() {
+        if seen.contains(&pkg) {
+            continue;
+        }
+
+        info!(log, "planning: {}", pkg);
+
+        let mats: Vec<_> = um.iter().filter(|m| &m.fmri == &pkg).collect();
+
+        if mats.is_empty() {
+            bail!("no match for FMRI {}", pkg);
+        } else if mats.len() > 1 {
+            bail!("{} matches for FMRI {}: {:?}", mats.len(), pkg, mats);
+        }
+
+        info!(log, "match: {:?}", mats[0]);
+
+        if mats[0].name == "illumos-gate" {
+            /*
+             * XXX
+             */
+            continue;
+        }
+
+        /*
+         * Check for this package in the build repository...
+         */
+        if !repo_contains(log, &format!("pkg:/{}", &pkg))? {
+            bail!("this package ({}) is not yet in the repo", pkg);
+        }
+
+        /*
+         * Get the dependencies for this package and put them in the queue...
+         */
+        let contents = repo_contents(log, &format!("pkg:/{}", &pkg))?;
+
+        for a in contents.iter() {
+            match &a {
+                Action::Depend(ad) => {
+                    if ad.type_ != "require" {
+                        bail!("unexpected depend type: {:?}", ad);
+                    }
+
+                    if seen.contains(&ad.fmri) {
+                        continue;
+                    }
+
+                    info!(log, "adding: {} -> {}", pkg, ad.fmri);
+                    q.push_back(ad.fmri.clone());
+                }
+                _ => {}
+            }
+        }
+    }
 
     Ok(())
 }
@@ -575,6 +921,12 @@ fn main() -> Result<()> {
         name: "archive".into(),
         desc: "archive".into(),
         func: cmd_archive,
+        hide: true,
+    });
+    handlers.push(CommandInfo {
+        name: "userland-plan".into(),
+        desc: "userland-plan".into(),
+        func: cmd_userland_plan,
         hide: true,
     });
 
