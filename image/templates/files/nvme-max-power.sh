31dd2cabb4d5ae28d4146c3ebd73b0926b439a3e
Dan Cross <cross@oxidecomputer.com>
2023-04-07 12:34:22 +0000

    image: clean up stress-test shell script

--- image/templates/files/nvme-max-power.sh
+++ image/templates/files/nvme-max-power.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
-
-
-diskinfo | grep "^NVME" | awk '{s=(NR==1?s:s ":") "/dev/dsk/" $2 "p0"} END {print s " " NR}' | read NVME_DEVICES NVME_COUNT
-
-fio /root/nvme-max-power.fio --filename ${NVME_DEVICES} --numjobs ${NVME_COUNT}
+#!/bin/ksh93
+set -A NVMES $(diskinfo | awk '/^NVME/ {print "/dev/dsk/"$2"p0"}')
+DEVS=$(IFS=':'; echo "${NVMES[*]}")
+NDEVS=${#NVMES[@]}
+fio /root/nvme-max-power.fio --filename "${DEVS}" --numjobs "${NDEVS}"
