8615d25473001cd992a14521fe600af9f8a95726
Dan Cross <cross@oxidecomputer.com>
2023-04-07 13:37:37 +0000

    image: diskinfo has useful options
    The last fiddling I'm going to do with this tiny shells cript.

--- image/templates/files/nvme-max-power.sh
+++ image/templates/files/nvme-max-power.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh93
-typeset -a NVMES=($(diskinfo | awk '/^NVME/ {print "/dev/dsk/"$2"p0"}'))
+typeset -a NVMES=($(diskinfo -Hp | awk '/^NVME/ {print "/dev/dsk/"$2"p0"}'))
 DEVS=$(IFS=':'; echo "${NVMES[*]}")
 NDEVS=${#NVMES[@]}
 fio /root/nvme-max-power.fio --filename "${DEVS}" --numjobs "${NDEVS}"
