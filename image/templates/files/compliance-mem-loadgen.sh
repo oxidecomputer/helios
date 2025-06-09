b46dcc70fd669f03ac3d089f5a8719e790f0e5d6
Dan Cross <cross@oxidecomputer.com>
2025-06-09 19:21:34 -0400

    hardware-cosmo#598 Need SMF service for compliance DRAM load (#204)
    Signed-off-by: Dan Cross <cross@oxidecomputer.com>

---
+++ image/templates/files/compliance-mem-loadgen.sh
@@ -0,0 +1,33 @@
+#!/bin/ksh93
+#
+# Copyright 2025 Oxide Computer Company
+#
+
+. /lib/svc/share/smf_include.sh
+
+function fatal {
+	echo "$@" >&2
+	exit $SMF_EXIT_ERR_FATAL
+}
+
+if (( $# != 1 )); then
+	fatal "usage: compliance-mem-loadgen <jobfile>"
+fi
+
+JOB_FILE=$1
+STRESS_NG=/opt/ooce/bin/stress-ng
+
+if [[ ! -x $STRESS_NG ]]; then
+	 fatal "Cannot find executable file $STRESS_NG"
+fi
+
+if [[ ! -f $JOB_FILE ]]; then
+	echo "compliance-mem-loadgen: Cannot find job file $JOB_FILE" >&2
+	exit $SMF_EXIT_ERR_CONFIG
+fi
+
+$STRESS_NG --job  $JOB_FILE &
+pid=$!
+kill -0 $pid || fatal "stress-ng did not start"
+
+exit $SMF_EXIT_OK
